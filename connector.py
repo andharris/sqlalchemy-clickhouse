d30e05ec53d50ccadd1de29a8c5c5ccbe7e44f4e
andrew <anharri2@cisco.com>
2020-01-15 20:24:39 -0500

    rely on session that includes auth when patching Database

--- connector.py
+++ connector.py
@@ -103,7 +103,7 @@ def _send(self, data, settings=None, stream=False):
     if PY3 and isinstance(data, string_types):
         data = data.encode('utf-8')
     params = self._build_params(settings)
-    r = requests.post(self.db_url, params=params, data=data, stream=stream, verify=False)
+    r = self.request_session.post(self.db_url, params=params, data=data, stream=stream, verify=False)
     if r.status_code != 200:
         raise Exception(r.text)
     return r
